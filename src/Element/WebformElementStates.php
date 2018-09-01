eaf2f0c741366e998f10c73903e3b796bda11463
jrockowitz <jrockowitz@gmail.com>
2018-08-31 22:19:30 -0400

    Review fix disable/enable #states warning message.

--- src/Element/WebformElementStates.php
+++ src/Element/WebformElementStates.php
@@ -199,7 +199,6 @@ class WebformElementStates extends FormElement {
       '#type' => 'webform_message',
       '#message_message' => t('<a href="https://www.w3schools.com/tags/att_input_disabled.asp" target="_blank">Disabled</a> elements do not submit data back to the server and the element\'s server-side default or current value will be preserved and saved to the database.'),
       '#message_type' => 'warning',
-      '#attributes' => ['style' => 'display:none'],
       '#states' => ['visible' => $triggers],
     ];
 
