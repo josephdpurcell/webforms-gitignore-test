ae28e895f96c8abbc0155edae4ee19e2b3ab87b5
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2018-09-13 14:32:41 -0400

    Issue #2998917 by jrockowitz: [Drupal 8.6.x] WebformHandlerBase and PluginBase both use the MessengerTrait;

--- src/Plugin/WebformHandlerInterface.php
+++ src/Plugin/WebformHandlerInterface.php
@@ -9,6 +9,7 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\PluginFormInterface;
 use Drupal\webform\WebformInterface;
 use Drupal\webform\WebformSubmissionInterface;
+use Psr\Http\Message\RequestInterface;
 
 /**
  * Defines the interface for webform handlers.
