64759e464cfe3abde994a7cda4e81e797c67f5ac
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2018-10-14 19:42:41 -0400

    Issue #3005493 by jrockowitz, facine: WebformHandlerFormBase::getUniqueMachineName always returns an empty suggestion

--- src/Form/WebformHandlerAddForm.php
+++ src/Form/WebformHandlerAddForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\webform\Form;
 
+use Drupal\Component\Transliteration\TransliterationInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\webform\Plugin\WebformHandlerManagerInterface;
 use Drupal\webform\WebformInterface;
@@ -24,13 +25,15 @@ class WebformHandlerAddForm extends WebformHandlerFormBase {
   /**
    * Constructs a WebformHandlerAddForm.
    *
+   * @param \Drupal\Component\Transliteration\TransliterationInterface $transliteration
+   *   The transliteration helper.
    * @param \Drupal\webform\WebformTokenManagerInterface $token_manager
    *   The webform token manager.
    * @param \Drupal\webform\Plugin\WebformHandlerManagerInterface $webform_handler
    *   The webform handler manager.
    */
-  public function __construct(WebformTokenManagerInterface $token_manager, WebformHandlerManagerInterface $webform_handler) {
-    parent::__construct($token_manager);
+  public function __construct(TransliterationInterface $transliteration, WebformTokenManagerInterface $token_manager, WebformHandlerManagerInterface $webform_handler) {
+    parent::__construct($transliteration, $token_manager);
     $this->webformHandlerManager = $webform_handler;
   }
 
@@ -39,6 +42,7 @@ class WebformHandlerAddForm extends WebformHandlerFormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
+      $container->get('transliteration'),
       $container->get('webform.token_manager'),
       $container->get('plugin.manager.webform.handler')
     );
