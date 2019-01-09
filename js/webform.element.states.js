4d6a5948b2811cfa48a13d5a660772605bec4491
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2019-01-09 17:15:50 -0500

    Issue #3024568 by jrockowitz: Conditional field requirement, customize requrement text

--- js/webform.element.states.js
+++ js/webform.element.states.js
@@ -14,7 +14,7 @@
    */
   Drupal.behaviors.webformElementStates = {
     attach: function (context) {
-      $(context).find('.webform-states-table--condition').once('webform-element-states').each(function () {
+      $(context).find('.webform-states-table--condition').once('webform-element-states-condition').each(function () {
         var $condition = $(this);
         var $selector = $condition.find('.webform-states-table--selector select');
         var $value = $condition.find('.webform-states-table--value input');
@@ -47,7 +47,46 @@
           }
         }).change();
       });
+
+      // If the states:state is required or optional the required checkbox
+      // should be checked and disabled.
+      var $state = $(context).find('.webform-states-table--state select');
+      if ($state.length) {
+        $state.once('webform-element-states-state')
+          .on('change', toggleRequiredCheckbox);
+        toggleRequiredCheckbox();
+      }
     }
   };
+
+  /**
+   * Toggle the required checkbox when states:state is required or optional.
+   */
+  function toggleRequiredCheckbox() {
+    var $input = $('input[name="properties[required]"]');
+    if (!$input.length) {
+      return;
+    }
+
+    // Determine if any states:state is required or optional.
+    var required = false;
+    $('.webform-states-table--state select').each(function () {
+      var value = $(this).val();
+      if (value === 'required' || value === 'optional') {
+        required = true;
+      }
+    });
+
+    if (required) {
+      $input.data('webform-states-required', $input.attr('checked') || false);
+      $input.attr('checked', true).attr('disabled', true);
+    }
+    else {
+      var checked = $input.hasData('webform-states-required') ? $input.data('webform-states-required') : false;
+      $input.removeData('webform-states-required');
+      $input.attr('checked', checked).attr('disabled', false);
+    }
+    $input.change();
+  }
 
 })(jQuery, Drupal, drupalSettings);
