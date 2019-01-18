4a5aba67cfc7af303f0cede4f2d9f8690f99db31
jrockowitz <jrockowitz@gmail.com>
2019-01-18 14:43:15 -0500

    Add Webform JSON Schema to Add-ons.

--- src/WebformAddonsManager.php
+++ src/WebformAddonsManager.php
@@ -731,6 +731,14 @@ class WebformAddonsManager implements WebformAddonsManagerInterface {
       'category' => 'rest',
     ];
 
+    // REST: Webform JSON Schema.
+    $projects['webform_jsonschema'] = [
+      'title' => $this->t('Webform JSON Schema'),
+      'description' => $this->t('Expose webforms as JSON Schema, UI Schema, and Form Data. Make webforms work with react-jsonschema-form.'),
+      'url' => Url::fromUri('https://github.com/AmazeeLabs/webform_jsonschema'),
+      'category' => 'rest',
+    ];
+
     /**************************************************************************/
     // Utility.
     /**************************************************************************/
