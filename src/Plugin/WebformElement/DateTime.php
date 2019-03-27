929a04aef7d732fd79bff6a9326b864eb216cea3
git <git@3426415.no-reply.drupal.org>
2019-03-26 20:08:09 -0400

    Issue #3043621 by fskreuz: Fix DateTime Element Step

--- src/Plugin/WebformElement/DateTime.php
+++ src/Plugin/WebformElement/DateTime.php
@@ -309,8 +309,8 @@ class DateTime extends DateBase {
       '#title' => $this->t('Time step'),
       '#description' => $this->t('Specifies the minute intervals.'),
       '#options' => [
-        '' => $this->t('1 minute'),
-        30 => $this->t('5 minutes'),
+        60 => $this->t('1 minute'),
+        300 => $this->t('5 minutes'),
         600 => $this->t('10 minutes'),
         900 => $this->t('15 minutes'),
         1200 => $this->t('20 minutes'),
