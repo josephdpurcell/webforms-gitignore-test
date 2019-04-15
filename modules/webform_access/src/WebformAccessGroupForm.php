e5ba20a7a8d94c5f3c4637d131f84c082206526e
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2019-04-15 16:12:18 -0400

    Issue #3048336 by thalles: Replace EntityManager by EntityTypeManagerInterface

--- modules/webform_access/src/WebformAccessGroupForm.php
+++ modules/webform_access/src/WebformAccessGroupForm.php
@@ -11,7 +11,7 @@ use Drupal\webform\Utility\WebformDialogHelper;
 use Drupal\webform\WebformAccessRulesManagerInterface;
 use Drupal\webform\WebformEntityReferenceManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Entity\EntityManager;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Provides a form to define a webform access group.
@@ -28,9 +28,9 @@ class WebformAccessGroupForm extends EntityForm {
   /**
    * Entity manager.
    *
-   * @var Drupal\Core\Entity\EntityManager
+   * @var Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected $entityManager;
+  protected $entityTypeManagerInterface;
 
   /**
    * The webform element manager.
@@ -58,7 +58,7 @@ class WebformAccessGroupForm extends EntityForm {
    *
    * @param \Drupal\Core\Database\Connection $database
    *   The database.
-   * @param \Drupal\Core\Entity\EntityManager $entity_manager
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity manager.
    * @param \Drupal\webform\Plugin\WebformElementManagerInterface $element_manager
    *   The webform element manager.
@@ -67,9 +67,9 @@ class WebformAccessGroupForm extends EntityForm {
    * @param \Drupal\webform\WebformAccessRulesManagerInterface $webform_access_rules_manager
    *   The webform access rules manager.
    */
-  public function __construct(Connection $database, EntityManager $entity_manager, WebformElementManagerInterface $element_manager, WebformEntityReferenceManagerInterface $webform_entity_reference_manager, WebformAccessRulesManagerInterface $webform_access_rules_manager) {
+  public function __construct(Connection $database, EntityTypeManagerInterface $entity_type_manager, WebformElementManagerInterface $element_manager, WebformEntityReferenceManagerInterface $webform_entity_reference_manager, WebformAccessRulesManagerInterface $webform_access_rules_manager) {
     $this->database = $database;
-    $this->entityManager = $entity_manager;
+    $this->entityTypeManager = $entity_type_manager;
     $this->elementManager = $element_manager;
     $this->webformEntityReferenceManager = $webform_entity_reference_manager;
     $this->webformAccessRulesManager = $webform_access_rules_manager;
@@ -81,7 +81,7 @@ class WebformAccessGroupForm extends EntityForm {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('database'),
-      $container->get('entity.manager'),
+      $container->get('entity_type.manager'),
       $container->get('plugin.manager.webform.element'),
       $container->get('webform.entity_reference_manager'),
       $container->get('webform.access_rules_manager')
@@ -418,7 +418,7 @@ class WebformAccessGroupForm extends EntityForm {
     $webforms = Webform::loadMultiple($webform_ids);
 
     /** @var \Drupal\node\NodeInterface[] $nodes */
-    $nodes = $this->entityManager->getStorage('node')->loadMultiple($nids);
+    $nodes = $this->entityTypeManager->getStorage('node')->loadMultiple($nids);
 
     $options = [];
     foreach ($webform_nodes as $nid => $field_names) {
