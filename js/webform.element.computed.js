12488086b28677b6ecfd5062911a79653d1261a6
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2019-05-15 14:41:47 -0400

    Issue #3053760 by jrockowitz, justinross: Ajax updating of a Computed Twig field referencing an array breaks when changing the number of items in the array

--- js/webform.element.computed.js
+++ js/webform.element.computed.js
@@ -11,6 +11,8 @@
   Drupal.webform.computed = Drupal.webform.computed || {};
   Drupal.webform.computed.delay = Drupal.webform.computed.delay || 500;
 
+  var computedElements = [];
+
   /**
    * Initialize computed elements.
    *
@@ -18,10 +20,15 @@
    */
   Drupal.behaviors.webformComputed = {
     attach: function (context) {
+      // Attach behaviors to computed elements.
       $(context).find('.js-webform-computed').once('webform-computed').each(function () {
-        // Get computed element and parent form.
+        // Get computed element and form.
         var $element = $(this);
         var $form = $element.closest('form');
+
+        // Get unique id for computed element based on the element name
+        // and form id.
+        var id = $form.attr('id') + '-' + $element.find('input[type="hidden"]').attr('name');
 
         // Get elements that are used by the computed element.
         var elementKeys = $(this).data('webform-element-keys').split(',');
@@ -37,11 +44,55 @@
           // Sub inputs. (aka #tree)
           inputs.push(':input[name^="' + key + '["]');
         });
-        var $triggers = $form.find(inputs.join(','));
+        var triggers = inputs.join(',');
 
+        // Track computed elements.
+        computedElements.push({
+          id: id,
+          element: $element,
+          form: $form,
+          triggers: triggers
+        });
+      });
+
+      // Initialize triggers for each computed element.
+      $.each(computedElements, function (index, computedElement) {
+        // Get trigger from the current context.
+        var $triggers = $(context).find(computedElement.triggers);
+        if (!$triggers.length) {
+          return;
+        }
+
+        // Make sure triggers are within the computed element's form and only
+        // initialized once.
+        $triggers = computedElement.form.find($triggers)
+          .once('webform-computed-triggers-' + computedElement.id);
+        if (!$triggers.length) {
+          return;
+        }
+
+        initializeTriggers(computedElement.element, $triggers);
+      });
+
+      /**
+       * Initialize computed element triggers.
+       *
+       * @param {jQuery} $element
+       *   An jQuery object containing the computed element.
+       * @param {jQuery} $triggers
+       *   An jQuery object containing the computed element triggers.
+       */
+      function initializeTriggers($element, $triggers) {
         // Add event handler to computed element triggers.
         $triggers.on('keyup change',
           debounce(triggerUpdate, Drupal.webform.computed.delay));
+
+        // Track tabledrag events.
+        var $draggable = $triggers.closest('tr.draggable');
+        if ($draggable.length) {
+          $draggable.find('.tabledrag-handle').on('mouseup pointerup touchend',
+            debounce(triggerUpdate, Drupal.webform.computed.delay));
+        }
 
         // Initialize computed element update which refreshes the displayed
         // value and accounts for any changes to the #default_value for a
@@ -67,7 +118,9 @@
           // Trigger computation.
           $element.find('.js-form-submit').mousedown();
         }
-      });
+
+      }
+
     }
   };
 
